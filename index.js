45b48fb19214823f0da74a929cbb6ec39f5ee931
Poomlak <76911820+Poomlak@users.noreply.github.com>
2024-10-12 11:02:01 +0000

    Hw6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 5000
 const bodyParser =require('body-parser');
 
+var mysql =require('mysql');
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use((req,res,next)=>{
@@ -19,27 +21,52 @@ app.use((req,res,next)=>{
 });
 
 
-app.use(express.json());
-let products=[
-  {id:0,name:"Notebook AcerSwift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-  {id:1,name:"Notebook AsusVivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-  {id:2,name:"Notebook LenovoIdeapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-  {id:3,name:"Notebook MSIPrestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-  {id:4,name:"Notebook DELLXPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-  {id:5,name:"Notebook HPEnvy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
 
+// let products=[
+//   {id:0,name:"Notebook AcerSwift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
+//   {id:1,name:"Notebook AsusVivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
+//   {id:2,name:"Notebook LenovoIdeapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
+//   {id:3,name:"Notebook MSIPrestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
+//   {id:4,name:"Notebook DELLXPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
+//   {id:5,name:"Notebook HPEnvy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
+
+var con = mysql.createConnection({
+  host : "korawit.ddns.net", 
+  user : "webapp", 
+  password : "secret2024", 
+  port : "3307", 
+  database : "shop"
+});
+con.connect(function(err){
+  if(err) throw err;
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/api/products',(req,res)=>{
-  if(products.length>0)
-    res.send(products);
-  else
-    res.status(400).send("No product founds");
+    con.query("SELECT * FROM products",function(err,result,fields){
+      if (err) throw res.status(400).send('Not found any products');
+      console.log(result);
+      res.send(result);
+    })
 });
 
+app.get('/api/products/:id',function(req,res){
+  const id = req.params.id;
+  con.query("SELECT * FROM products where id="+id, function(err,result,fields){
+    if(err) throw err;
+    let products=result;
+    if(products.length>0){
+      res.send(products);
+    }
+    else{
+      res.status(400).send('Not found product for'+id);
+    }
+    console.log(result);
+  });
+});
 
 
 
